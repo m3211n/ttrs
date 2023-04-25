d6c51905c8142ac17c407c789be7a3f365cbcbfb
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-25 21:51:59 +0200

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 class tetrisGrid {
     
     grid: number[][]
+    next: number[][]
+
+    score: number
+    lines: number
 
     private spritesGrid: Sprite[][]
+    private nextGrid: Sprite[][]
+    private scoreSprite: TextSprite
+    private linesSprite: TextSprite
 
     constructor () {
         this.grid = []
@@ -19,6 +26,29 @@ class tetrisGrid {
             this.grid.push(row)
             this.spritesGrid.push(s_row)
         }
+
+        this.next = []
+        this.nextGrid = []
+        for (let i = 0; i < 4; i++) {
+            let row: number[] = []
+            let s_row: Sprite[] = []
+            for (let j = 0; j < 4; j++) {
+                row.push(0)
+                let cell = sprites.create(assets.image`color7`, SpriteKind.Player)
+                cell.setPosition(j * 6 + 127, i * 6 + 25)
+                s_row.push(cell)
+            }
+            this.next.push(row)
+            this.nextGrid.push(s_row)
+        }
+
+        this.scoreSprite = textsprite.create("0")
+        this.scoreSprite.setMaxFontHeight(10)
+        this.scoreSprite.setPosition(137, 72)
+
+        this.linesSprite = textsprite.create("0")
+        this.linesSprite.setMaxFontHeight(10)
+        this.linesSprite.setPosition(137, 105)
     }
 
     update () {
@@ -42,7 +72,44 @@ class tetrisGrid {
             }
             
         }
+
+        this.scoreSprite.setText(this.score.toString())
+        this.linesSprite.setText(this.lines.toString())
     }
+
+    scanRows () {
+
+        let result: boolean = false
+        for (let i = 0; i < 20; i++) {
+            let row: number[] = []
+            for (let j = 0; j < 10; j++) {
+                row.push(0)
+            }
+            if (this.grid[i].indexOf(0) == -1) {
+                this.grid.removeAt(i)
+                this.grid.unshift(row)
+                result = true
+            }
+        }
+        return result
+    }
+
+}
+
+class tetrisPiece {
+
+    shape: number
+    rotation: number
+    posX: number
+    posY: number
+
+    constructor (s: number, r: number, speed: number) {
+        this.shape = s
+        this.rotation = r
+        this.posX = 5
+        this.posY = 0
+    }
+    
 }
 
 scene.setBackgroundImage(assets.image`game`)
