3f73a152a5609ea66dfd02790abfdb76d668b9b1
m3211n <130044007+m3211n@users.noreply.github.com>
2023-04-29 01:58:40 +0200

    Update main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,204 +1,74 @@
-class tetrisPiece {
-    pieces: number [][][] = [
-        [[4, 5, 6, 7], [2, 6, 10, 14], [8, 9, 10, 11], [1, 5, 9, 13]],
-        [[0, 4, 5, 6], [1, 2, 5, 9], [4, 5, 6, 10], [1, 5, 8, 9]],
-        [[2, 4, 5, 6], [1, 5, 9, 10], [4, 5, 6, 8], [0,1, 5, 9]],
-        [[1, 2, 5, 6], [1, 2, 5, 6], [1, 2, 5, 6], [1, 2, 5, 6]],
-        [[1, 2, 4, 5], [1, 5, 6, 10], [5, 6, 8, 9], [0, 4, 5, 9]],
-        [[1, 4, 5, 6], [1, 5, 6, 9], [4, 5, 6, 9], [1, 4, 5, 9]],
-        [[0, 1, 5, 6], [2, 5, 6, 9], [4, 5,9, 10], [1, 4, 5, 8]]
-    ]
-
-    shape: number
+class Tetrimino {
+    matrix: Sprite[][]
+    shapeID: number
     rotation: number
-
     row: number
     col: number
 
-    matrix: Sprite[][]
-   
-    constructor (s: number) {
-        this.shape = s
+    constructor (shape: number) {
+        this.shapeID = shape
         this.rotation = 0
         this.matrix = []
-
-        for (let i = 0; i < 4; i++) {
-            let r: Sprite[] = []
-            for (let j = 0; j < 4; j++) {
-                r.push(null)
-            }
-            this.matrix.push(r)
-        }
-        for (let i = 0; i < 4; i++) {
-            let x = this.pieces[this.shape][this.rotation][i] % 4
-            let y = Math.floor(this.pieces[this.shape][this.rotation][i] / 4)
-            this.matrix[y][x] = sprites.create(color[this.shape], SpriteKind.Player)
-        }
-        this.setLocation(0, 3)
-    }
-
-    setLocation (r: number, c: number) {
-        this.row = r
-        this.col = c
-
-        for (let i = 0; i < 4; i++) {
-            let x = this.pieces[this.shape][this.rotation][i] % 4
-            let y = Math.floor(this.pieces[this.shape][this.rotation][i] / 4)
-            this.matrix[y][x].setPosition((x + this.col) * 6 + FIELD_X, (y + this.row) * 6 + FIELD_Y)
-        }
-    }
-
-    placeNext () {
-        for (let i = 0; i < 4; i++) {
-            let x = this.pieces[this.shape][this.rotation][i] % 4
-            let y = Math.floor(this.pieces[this.shape][this.rotation][i] / 4)
-            this.matrix[y][x].setPosition(x * 6 + 129, y * 6 + 21)
-        }
-    }
-}
-
-class tetrisGame {
-
-    piece: tetrisPiece
-    field: Sprite[][]
-    next: Sprite[][]
-    hold: Sprite[][]
-
-    nextQueue: number[]
-    holdQueue: number[]
-
-    level: number
-    score: number
-    gravity: number
-
-    private levelTS: TextSprite
-    private scoreTS: TextSprite
-
-    constructor () {
-        this.level = 1
-        this.score = 0
-        this.gravity = Math.pow(0.8 - ((this.level - 1) * 0.007), this.level - 1)
-
-        this.field = []
-        this.next = []
-
-        for (let r = 0; r < 20; r++) {
+        let i = 0
+        for (let r = 0; r < 4; r ++) {
             let row: Sprite[] = []
-            for (let c = 0; c < 10; c++) {
-                row.push(null)
-            }
-            this.field.push(row)
-        }
-
-        for (let i = 0; i < 2; i++) {
-            let row: Sprite[] = []
-            for (let j = 0; j < 4; j++) {
-                row.push(null)
-            }
-            this.next.push(row)
-        }
-
-        this.levelTS = textsprite.create(this.level.toString())
-        this.levelTS.setPosition(138, 77)
-        this.levelTS.setMaxFontHeight(8)
-
-        this.scoreTS = textsprite.create(this.score.toString())
-        this.scoreTS.setPosition(138, 101)
-        this.scoreTS.setMaxFontHeight(8)
-
-        this.nextQueue = []
-        this.nextQueue.push(randomizer())
-        this.spawn()
-
-    }
-
-    getNextPiece () : number {
-        this.nextQueue.push(randomizer())
-        let n = this.nextQueue.shift()
-        for (let r = 0; r < 2; r++) {
-            for (let c = 0; c < 4; c++) {
-                if (this.next[r][c] != null) {
-                    this.next[r][c].destroy()
+            for (let c = 0; c < 4; c ++) {
+                if (shapes[this.shapeID][this.rotation][i] % 4 == c && Math.floor(shapes[this.shapeID][this.rotation][i]/4) == r) {
+                    let tmpSprite = sprites.create(colors[this.shapeID])
+                    tmpSprite.setPosition(c * 5, r * 5)
+                    row.push(tmpSprite)
+                    i++
+                } else {
+                    row.push(null)
                 }
             }
+            this.matrix.push(row)
         }
-        this.updateNext()
-        return n
     }
 
-    spawn() {
-        let p = this.getNextPiece()
-        let piece = new tetrisPiece(p)
-        this.piece = piece
-        let locked = false
-    }
-
-    lock () {
+    setPosition(col: number, row: number) {
+        this.col = col
+        this.row = row
         for (let r = 0; r < 4; r ++) {
             for (let c = 0; c < 4; c ++) {
-                let l_row = this.piece.row + r
-                let l_col = this.piece.col + c
-                this.field[l_row][l_col] = this.piece.matrix[r][c]
-            }
-        }
-        this.spawn()
-    }
-
-    updateNext() {
-        let nextPiece = new tetrisPiece(this.nextQueue[0])
-        for (let r = 0; r < 2; r ++) {
-            for (let c = 0; c < 4; c++) {
-                nextPiece.placeNext()
-                this.next[r][c] = nextPiece.matrix[r][c]
-            }
-        }
-    }
-
-    softDrop(reverse: boolean) {
-        this.gravity = reverse ? Math.pow(0.8 - ((this.level - 1) * 0.007), this.level - 1) : 0.05
-        console.log ("Reverse = " + reverse + ", gravity is " + this.gravity)
-    }
-}
-
-function randomizer () : number {
-    let n = 0
-    if (Math.percentChance(10.7)) {
-        n = 2
-    } else {
-        if (Math.percentChance(13.7)) {
-            n = 0
-        } else {
-            if (Math.percentChance(13.7)) {
-                n = 6
-            } else { 
-                if (Math.percentChance(13.7)) {
-                    n = 1
-                } else {
-                    if (Math.percentChance(16.1)) {
-                        n = 3
-                    } else {
-                        if (Math.percentChance(16.1)) {
-                            n = 4
-                        } else {
-                            n = 5
-                        }
-                    }
+                if (this.matrix[r][c] != null) {
+                    this.matrix[r][c].setPosition((this.col + c) * 5 + X0, (this.row + r) * 5 + Y0) 
                 }
             }
         }
     }
-    return n
-} 
 
-function moveDown() {
-    let r = tetris.piece.row
-    let c = tetris.piece.col
-    r++
-    tetris.piece.setLocation(r, c)
+    setRotation (rotation: number) {
+        this.rotation = rotation
+        let i = 0
+        for (let r = 0; r < 4; r++) {
+            for (let c = 0; c < 4; c++) {
+                if (this.matrix[r][c] != null) {
+                    this.matrix[r][c].destroy()
+                    this.matrix[r][c] = null
+                }
+                if (shapes[this.shapeID][this.rotation][i] % 4 == c && Math.floor(shapes[this.shapeID][this.rotation][i] / 4) == r) {
+                    let tmpSprite = sprites.create(colors[this.shapeID])
+                    tmpSprite.setPosition((this.col + c) * 5 + X0, (this.row + r) * 5 + Y0)
+                    this.matrix[r][c] = tmpSprite 
+                    i++
+                }
+            }
+        }
+    }
 }
 
+const shapes = [
+    [[4, 5, 6, 7], [2, 6, 10, 14], [8, 9, 10, 11], [1, 5, 9, 13]],
+    [[0, 4, 5, 6], [1, 2, 5, 9], [4, 5, 6, 10], [1, 5, 8, 9]],
+    [[2, 4, 5, 6], [1, 5, 9, 10], [4, 5, 6, 8], [0, 1, 5, 9]],
+    [[1, 2, 5, 6], [1, 2, 5, 6], [1, 2, 5, 6], [1, 2, 5, 6]],
+    [[1, 2, 4, 5], [1, 5, 6, 10], [5, 6, 8, 9], [0, 4, 5, 9]],
+    [[1, 4, 5, 6], [1, 5, 6, 9], [4, 5, 6, 9], [1, 4, 5, 9]],
+    [[0, 1, 5, 6], [2, 5, 6, 9], [4, 5, 9, 10], [1, 4, 5, 8]]
+]
 
-const color: Image[] = [
+const colors: Image[] = [
     assets.image`color0`,
     assets.image`color1`,
     assets.image`color2`,
@@ -208,34 +78,24 @@ const color: Image[] = [
     assets.image`color6`
 ]
 
-const FIELD_X = 54
-const FIELD_Y = 3
-
-let gravityID
-
-let tick: boolean = true
+const X0 = 56
+const Y0 = 8
 
 scene.setBackgroundImage(assets.image`game`)
 
-let tetris = new tetrisGame()
+let t = new Tetrimino(5)
+t.setPosition(3, 0)
 
-game.onUpdate( function() {
-    let r = tetris.piece.row
-    let c = tetris.piece.col
-    setInterval(function () {
-        r++
-        tetris.piece.setLocation(r, c)
-    }, 1000 * tetris.gravity)
-    console.log(tetris.gravity)
+controller.down.onEvent(ControllerButtonEvent.Repeated, function() {
+    let c = t.col
+    let r = t.row
+    r++
+    t.setPosition(c, r)
 })
 
-
-controller.down.onEvent(ControllerButtonEvent.Pressed, function() {
-    tetris.softDrop(false)
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    let c = t.col
+    let r = t.row
+    r++
+    t.setPosition(c, r)
 })
-
-controller.down.onEvent(ControllerButtonEvent.Released, function () {
-    tetris.softDrop(true)
-})
-
-
